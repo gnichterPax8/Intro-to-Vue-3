2543ef43ac2c58c2a7999be1a4db58b72a6d9af3
Grant Nichter <gnichter@pax8.com>
2022-06-01 11:08:17 -0500

    first commit

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data(){
+        return {
+            cart: 0,
+            product: 'socks',
+            image: './assets/images/socks_green.jpg',
+            invin: 0,
+            dets: ['50% cotton', '30% wool', '20% polyester', 'and 100% not real'],
+            Vair: [
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
+                { id: 2235, color: 'blue', image: ' ./assets/images/socks_blue.jpg'},
+            ]
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateImage(VairImage) {
+            this.image = VairImage
+        }
+    }
+})
